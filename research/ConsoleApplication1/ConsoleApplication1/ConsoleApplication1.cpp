27fda41a43dd742c433192b0be9344678c889847
Azalea <clearchengli@yahoo.com>
2015-02-09 01:31:49 -0800

    Added signature checking and cleaned up the code

--- research/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ research/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,57 +1,89 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+
 #include <iostream>
+#include <cassert>
+#include <string>
 
 using namespace std;
 
+enum StateName{
+	STATE_INP, STATE_OUT, STATE_EVEN, STATE_ODD
+};
+
+enum Signature {
+	SIG_INP, SIG_ODD, SIG_EVEN, SIG_OUT
+};
+
 struct stateMachine{
-	bool inBool;
-	bool outBool;
-	bool isEven;
-	//stateMachine();
+	StateName stateName;
 	int signature;
+	int difference; //xor result of this state's signature and next state's signature
 	stateMachine* nextState;
 };
 
 int global;
-stateMachine inp = {1, 0, 0, 1};
-stateMachine odd = {0, 0, 0, 2};
-stateMachine even = {0, 0, 1, 3};
-stateMachine out = {0, 1, 0, 4};
+int G = SIG_INP;
+string output;
+
+stateMachine inp = {STATE_INP,
+					SIG_INP,
+					SIG_INP ^ SIG_OUT};
+
+stateMachine odd = {STATE_ODD,
+					SIG_ODD,
+					SIG_ODD ^ SIG_INP};
+
+stateMachine even = {STATE_EVEN,
+					SIG_EVEN,
+					SIG_EVEN ^ SIG_INP};
+
+stateMachine out = {STATE_OUT,
+					SIG_OUT,
+					SIG_OUT ^ SIG_EVEN};
 
 void driveState(stateMachine& state){
-	//G = G xor d1
 
-	if(state.inBool){
-		cin >> global;
-		if(global % 2){
-			state.nextState = &odd;
-		}
-		else
-			state.nextState = &even;
-			
-	}
-	else if(state.outBool){
-		cout << global << endl;
-		state.nextState = &inp;
-	}
-	else if(state.isEven){
-		global = 99;
-		state.nextState = &out;
-	}
-	else{
-		global = -1;
-		state.nextState = &out;
+	switch(state.stateName){
+		case(STATE_INP) :
+			cin >> global;
+			if(global % 2) {
+				state.nextState = &odd;
+				G = G ^ state.nextState->difference;
+			}
+			else {
+				state.nextState = &even;
+				G = G ^ state.nextState->difference;
+			}
+			break;
+
+		case(STATE_OUT) :
+			cout << output << endl;
+			state.nextState = &inp;
+			G = G ^ state.nextState->difference;
+			break;
+
+		case(STATE_EVEN) :
+			output = "EVEN";
+			state.nextState = &out;
+			G = (G ^ state.nextState->difference) ^ (0); //fan-in : xor'ed with extra difference variable
+			break;
+
+		case(STATE_ODD) :
+			output = "ODD";
+			state.nextState = &out;
+			G = (G ^ state.nextState->difference) ^ (even.signature ^ odd.signature); //fan-in : xor'ed with extra difference variable
+			break;
+
 	}
 
-	//ASSERT G == s1
+	assert(G == state.nextState->signature);
 	
 }
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	stateMachine state = inp;
 
