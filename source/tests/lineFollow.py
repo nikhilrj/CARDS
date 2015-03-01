fb329f83cd7eab80193bc809137868d074921f36
Nikhil Joglekar <nikhiljoglekar@uteas.edu>
2015-03-01 14:54:14 -0600

    lineFollowing initial

---
+++ source/tests/lineFollow.py
@@ -0,0 +1,73 @@
+import RPi.GPIO as GPIO
+import sys,tty,termios,time
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)   
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+fl = mh.getMotor(1)
+fr = mh.getMotor(2)
+bl = mh.getMotor(3)
+br = mh.getMotor(4)
+
+motors = [fl, fr, bl, br]
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(27, GPIO.IN)
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
+
+baseSpeed = 50
+
+def sensorRead():
+	#returns sensor read data as an array
+	return [GPIO.input(4), GPIO.input(17), GPIO.input(27), GPIO.input(22), GPIO.input(23), GPIO.input(24)]
+
+
+def calcWeights(sensorData, c=4):
+	weights = [-12, -6, 0, 0, 6, 12]
+
+	numActive = 0
+	for i in sensorData:
+		if i > 0:
+			numActive+=1
+
+	dot = 0
+	for i in xrange(0, len(weights)):
+		dot += weights[i] * sensorData[i]
+
+	weight = dot / numActive
+
+	return [baseSpeed + c*weight, baseSpeed - c*weight, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.FORWARD]
+
+def driveMotors(lSpeed=baseSpeed, rSpeed=baseSpeed, lDir = Adafruit_MotorHAT.FORWARD, rDir = Adafruit_MotorHAT.FORWARD):
+	fl.setSpeed(lSpeed)
+	bl.setSpeed(lSpeed)
+	fr.setSpeed(rSpeed)
+	br.setSpeed(rSpeed)
+
+	fl.run(lDir)
+	bl.run(lDir)
+	fr.run(rDir)
+	br.run(rDir)
+
+while(True):
+	sensorData = sensorRead()
+	[lSpeed, rSpeed, lDir, rDir] = calcWeights(sensorData)
+	driveMotors(sensorData)
--- source/tests/pintest.py
+++ source/tests/pintest.py
@@ -1,50 +1,46 @@
 import RPi.GPIO as GPIO
 import sys,tty,termios,time
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(11, GPIO.IN)
-GPIO.setup(13, GPIO.IN)
-GPIO.setup(15, GPIO.IN)
-GPIO.setup(29, GPIO.IN)
-GPIO.setup(31, GPIO.IN)
-GPIO.setup(22, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
 GPIO.setup(18, GPIO.IN)
-GPIO.setup(16, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(27, GPIO.IN)
 
-pin11 = 0
-pin13 = 0
-pin15 = 0
-pin16 = 0
+pin4 = 0
+pin17 = 0
 pin18 = 0
+pin27 = 0
 pin22 = 0
-pin29 = 0
-pin31 = 0
+pin23 = 0
+pin24 = 0
 
 count = float(0)
 
 while(True):
 	if(count < 1000):
-		pin11 += GPIO.input(11)
-		pin13 += GPIO.input(13) 
-		pin15 += GPIO.input(15)
-		pin16 += GPIO.input(16) 
+		pin4 += GPIO.input(4)
+		pin17 += GPIO.input(17) 
 		pin18 += GPIO.input(18)
+		pin27 += GPIO.input(27) 
 		pin22 += GPIO.input(22) 
-		pin29 += GPIO.input(29)
-		pin31 += GPIO.input(31) 
+		pin23 += GPIO.input(23)
+		pin24 += GPIO.input(24) 
 		count += 1
 	else:
-		print pin11, pin13, pin15, pin16, pin18, pin22, pin29, pin31
+		print pin4, pin17, pin18, pin27, pin22, pin23, pin24
 		count = 0
-		pin11 = 0
-		pin13 = 0
-		pin15 = 0
-		pin16 = 0
+		pin4 = 0
+		pin17 = 0
 		pin18 = 0
+		pin27 = 0
 		pin22 = 0
-		pin29 = 0
-		pin31 = 0
+		pin23 = 0
+		pin24 = 0
 
 
 	
