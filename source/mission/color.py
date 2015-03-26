96fb56d9eb8616ec7bac9ed066e7a068c8a71a9d
Anurag Banerjee <Anurag.Banerjee@utexas.edu>
2015-03-26 17:41:26 -0500

    i dunno mission stuff
    added sensor read

--- source/mission/color.py
+++ source/mission/color.py
@@ -2,17 +2,21 @@
 
 import smbus, time
 
-bus = smbus.SMBus(1)
-i2cAdr = 0x29;
 
-bus.write_byte(i2cAdr,0x80|0x12)
-ver = bus.read_byte(i2cAdr)
+def colorInit():
+	bus = smbus.SMBus(1)
+	i2cAdr = 0x29;
+	blackThreshold = 5000;
+	whiteThreshold = 20000;
 
-if ver == 0x44:
-	print "Device found\n"
-	bus.write_byte(i2cAdr, 0x80|0x00) # 0x00 = ENABLE register
-	bus.write_byte(i2cAdr, 0x01|0x02) # 0x01 = Power on, 0x02 RGB sensors enabled
-	bus.write_byte(i2cAdr, 0x80|0x14) # Reading results start register 14, LSB then MSB
+	bus.write_byte(i2cAdr,0x80|0x12)
+	ver = bus.read_byte(i2cAdr)
+
+	if ver == 0x44:
+		print "Device found\n"
+		bus.write_byte(i2cAdr, 0x80|0x00) # 0x00 = ENABLE register
+		bus.write_byte(i2cAdr, 0x01|0x02) # 0x01 = Power on, 0x02 RGB sensors enabled
+		bus.write_byte(i2cAdr, 0x80|0x14) # Reading results start register 14, LSB then MSB
 	
 	
 
@@ -26,9 +30,9 @@ def readColor():
 	
 def distance(colorReading):
 	color = ''
-	if (colorReading[0] < 5000):
+	if (colorReading[0] < blackThreshold):
 		color = 'black'
-	elif (colorReading[0] > 20000):
+	elif (colorReading[0] > whiteThreshold):
 		color = 'white'
 	else:
 		if (colorReading[1] > colorReading[3]):
--- source/mission/mission.py
+++ source/mission/mission.py
@@ -9,8 +9,13 @@ from direction import *
 import os, atexit, time
 
 
+def sensorRead():
+	#returns sensor read data as an array
+	return [GPIO.input(17), GPIO.input(18), GPIO.input(27), GPIO.input(22), GPIO.input(23), GPIO.input(24)]
+
 
 while(True):
 	#Make calls to other files
+	
 	
 
