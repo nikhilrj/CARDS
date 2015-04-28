c93510573c53f1f40addd4691988598c27029f84
436C69 <clearchengli@yahoo.com>
2015-04-28 00:35:58 -0500

    Merged server pc with website functionality

--- source/laptop/serverpc.py
+++ source/laptop/serverpc.py
@@ -1,9 +1,11 @@
+from selenium import webdriver
 import socket
 import time
-import miniRSA
 import sys
 import rsa
 import threading
+import webbrowser
+import os.path
 
 # Settings for the client
 broadcastIP = '192.168.0.102'		 # IP address to send to, 255 in one or more positions is a broadcast / wild-card
@@ -15,27 +17,81 @@ size = 2048
 sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	 # Create the socket
 
 sender.connect((broadcastIP, broadcastPort)) # Set the IP and port number to use locally, IP 0.0.0.0 means all connections and port 0 means assign a number for us (do not care)
-print 'connected'
+print ('connected')
 ### Process of Receiving Public Key ###
 pubKeyN = sender.recv(size)
-print pubKeyN
+print (pubKeyN)
 pubKeyN = long(pubKeyN)
 time.sleep(1)
 pubKeyE = sender.recv(size)
-print pubKeyE
+print (pubKeyE)
 pubKeyE = long(pubKeyE)
 
 
 pubKey = rsa.PublicKey(pubKeyN,pubKeyE)
 
-print 'Public Key received from server.'
+print ('Public Key received from server.')
 
+driver = webdriver.Firefox()
+driver.get("file:///" + os.path.abspath('ConsoleAppWebpage.html'))
 
+pageTemplate = '''
+<!DOCTYPE html>
+<!-- saved from url=(0051)http://getbootstrap.com/examples/starter-template/# -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>CARDS</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="http://getbootstrap.com/examples/starter-template/starter-template.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+	<div class="container">
+
+	  <div class="starter-template">
+		<h1>CARDS</h1>
+		<p1 class="lead">Fault Injected: {num_injected}<br></p1>
+		<p2 class="lead">Memory Error Detected: {num_memerr}<br></p2>
+		<p3 class="lead">Control Flow Error Detected: {num_cfgerr}<br></p3>
+		<img id="img1" src="flow_chart.png" width="800" height="800">
+	  </div>
+
+	</div>
+
+</body></html>
+'''
+
+def strToFile(text, filename):
+	youtput = open(filename,"w")
+	output.write(text)
+	output.close()
+
+def browseLocal(webpageText, filename='ConsoleAppWebpage.html'):
+	strToFile(webpageText, filename)
+	driver.refresh()
 
 def worker():
 	while(True):
-		print(sender.recv(size))
-	
+		raw_msg = sender.recv(size)
+		str_msg = raw_msg.decode('utf-8')
+		num_injected = "0"
+		num_memerr = "0"
+		num_cfgerr = "0"
+
+		num_injected = str_msg
+		contents = pageTemplate.format(**locals())
+
+		browseLocal(contents)
+		
 	
 	
 time.sleep(2)
@@ -59,5 +115,3 @@ try:
 		#sender.sendto('ALLOFF', (broadcastIP, broadcastPort))
 except KeyboardInterrupt:
 	sender.sendto('ALLOFF', (broadcastIP, broadcastPort))
-	
-	
